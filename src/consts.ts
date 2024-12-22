ec068721d74fca386f88161c3ed4f4a72263b6eb
alcajorV2 <worksforalcajorv2@outlook.com>
2024-12-22 19:30:39 +0100

    Update of principal page.

--- src/consts.ts
+++ src/consts.ts
@@ -2,14 +2,14 @@ import type { Site, Page, Links, Socials } from "@types"
 
 // Global
 export const SITE: Site = {
-  TITLE: "Astro Sphere",
-  DESCRIPTION: "Welcome to Astro Sphere, a portfolio and blog for designers and developers.",
-  AUTHOR: "Mark Horn",
+  TITLE: "AlcajorV2",
+  DESCRIPTION: "Welcome to my website, here you can find my work, blog posts, and projects.",
+  AUTHOR: "Alca Jor",
 }
 
 // Work Page
 export const WORK: Page = {
-  TITLE: "Work",
+  TITLE: "Work Trayectorie",
   DESCRIPTION: "Places I have worked.",
 }
 
@@ -56,26 +56,26 @@ export const SOCIALS: Socials = [
   { 
     NAME: "Email",
     ICON: "email", 
-    TEXT: "markhorn.dev@gmail.com",
-    HREF: "mailto:markhorn.dev@gmail.com",
+    TEXT: "<empty>",
+    HREF: "mailto:<empty>",
   },
   { 
     NAME: "Github",
     ICON: "github",
-    TEXT: "markhorn-dev",
-    HREF: "https://github.com/markhorn-dev/astro-sphere"
+    TEXT: "AlcajorV2",
+    HREF: "https://github.com/alcajorv2"
   },
   { 
     NAME: "LinkedIn",
     ICON: "linkedin",
-    TEXT: "markhorn-dev",
-    HREF: "https://www.linkedin.com/in/markhorn-dev/",
+    TEXT: "<empty>",
+    HREF: "https://www.linkedin.com/in/<empty>/",
   },
   { 
     NAME: "Twitter",
     ICON: "twitter-x",
-    TEXT: "markhorn_dev",
-    HREF: "https://twitter.com/markhorn_dev",
+    TEXT: "<empty>",
+    HREF: "https://twitter.com/<empty>",
   },
 ]
 
